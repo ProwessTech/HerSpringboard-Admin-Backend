101ead5aaab36fd3cd04711341221800e705d8f6
Arul Frances <arulfrances@gmail.com>
2025-09-20 16:29:17 +0530

    Updated for Vercel

--- backend/server.js
+++ backend/server.js
@@ -244,6 +244,6 @@ app.delete('/courses/:courseId', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
